9bb7009d6debb5e501bf78d861b86a8b1471d424
jrgriffiniii <griffinj@lafayette.edu>
2014-03-28 00:07:48 -0400

    DSS-223 DSS-224 #time 7m Relabeling "Relation.SeeAlso" and rendering "Description.Text.English" for Object metadata

--- template.php
+++ template.php
@@ -676,7 +676,7 @@ function bootstrap_dss_digital_preprocess_islandora_book_book(array &$variables)
       $mods_str = preg_replace('/<\?xml .*?\?>/', '', $mods_str);
       //$mods_str = '<modsCollection>' . $mods_str . '</modsCollection>';
 
-      //dpm($mods_str);
+      dpm($mods_str);
       //dpm(islandora_solr_get_fields('result_fields', FALSE));
 
       $mods_object = new DssMods($mods_str);
